28d88b7ee86264ca77c27048ce29075ab92fba42
Robert Snyder <immeraufdemhund@hotmail.com>
2015-03-19 13:49:18 -0400

    Added basic implementation for a properties class

--- Upos.ServiceObject/Upos.ServiceObject/Base/Properties/IUposProperties.cs
+++ Upos.ServiceObject/Upos.ServiceObject/Base/Properties/IUposProperties.cs
@@ -1,5 +1,15 @@
-﻿namespace Upos.ServiceObject.Base.Properties
+﻿using System.Collections.Generic;
+
+namespace Upos.ServiceObject.Base.Properties
 {
+    public class IUposPropertiesFactory
+    {
+        public static IUposProperties Create()
+        {
+            return new UposBaseProperties();
+        }
+    }
+
     public interface IUposProperties
     {
         IUposBase ServiceObject { get; }
@@ -10,4 +20,55 @@
         string GetStringProperty(int propertyIndex);
         void SetStringProperty(int propertyIndex, string propertyValue);
     }
+
+    internal class UposBaseProperties : IUposProperties
+    {
+        private readonly Dictionary<int, object> propDictionary;
+
+        public UposBaseProperties()
+            : this(new Dictionary<int, object>())
+        {
+            propDictionary.Add(PropertyConstants.PIDX_ResultCode, 0);
+        }
+
+        public UposBaseProperties(Dictionary<int, object> propDictionary)
+        {
+            this.propDictionary = propDictionary;
+        }
+
+        public IUposBase ServiceObject
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int GetIntProperty(int propertyIndex)
+        {
+            if (propDictionary.ContainsKey(propertyIndex))
+                return (int)propDictionary[propertyIndex];
+
+            SetIntProperty(PropertyConstants.PIDX_ResultCode, (int)ResultCodeConstants.OPOS_E_FAILURE);
+            return -1;
+        }
+
+        public void SetIntProperty(int propertyIndex, int propertyValue)
+        {
+            SetProperty(propertyIndex, propertyValue);
+        }
+
+        public string GetStringProperty(int propertyIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetStringProperty(int propertyIndex, string propertyValue)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void SetProperty(int propertyIndex, object propertyValue)
+        {
+            propDictionary[propertyIndex] = propertyValue;
+        }
+    }
+
 }
---
+++ Upos.ServiceObject/Upos.ServiceObject/Base/ResultCodeConstants.cs
@@ -0,0 +1,36 @@
+﻿namespace Upos.ServiceObject.Base
+{
+    public enum ResultCodeConstants : int
+    {
+        OPOSERR = 100, // Base for ResultCode errors.
+        OPOSERREXT = 200, // Base for ResultCodeExtendedErrors.
+
+        OPOS_SUCCESS = 0,
+        OPOS_E_CLOSED = 1 + OPOSERR,
+        OPOS_E_CLAIMED = 2 + OPOSERR,
+        OPOS_E_NOTCLAIMED = 3 + OPOSERR,
+        OPOS_E_NOSERVICE = 4 + OPOSERR,
+        OPOS_E_DISABLED = 5 + OPOSERR,
+        OPOS_E_ILLEGAL = 6 + OPOSERR,
+        OPOS_E_NOHARDWARE = 7 + OPOSERR,
+        OPOS_E_OFFLINE = 8 + OPOSERR,
+        OPOS_E_NOEXIST = 9 + OPOSERR,
+        OPOS_E_EXISTS = 10 + OPOSERR,
+        OPOS_E_FAILURE = 11 + OPOSERR,
+        OPOS_E_TIMEOUT = 12 + OPOSERR,
+        OPOS_E_BUSY = 13 + OPOSERR,
+        OPOS_E_EXTENDED = 14 + OPOSERR,
+        OPOS_E_DEPRECATED = 15 + OPOSERR, // (added in 1.11)
+
+
+        /////////////////////////////////////////////////////////////////////
+        // OPOS "ResultCodeExtended" Property Constants
+        /////////////////////////////////////////////////////////////////////
+
+        // The following applies to ResetStatistics and UpdateStatistics.
+        OPOS_ESTATS_ERROR = 80 + OPOSERREXT, // (added in 1.8)
+        OPOS_ESTATS_DEPENDENCY = 82 + OPOSERREXT, // (added in 1.10)
+        // The following applies to CompareFirmwareVersion and UpdateFirmware.
+        OPOS_EFIRMWARE_BAD_FILE = 81 + OPOSERREXT, // (added in 1.9)
+    }
+}
