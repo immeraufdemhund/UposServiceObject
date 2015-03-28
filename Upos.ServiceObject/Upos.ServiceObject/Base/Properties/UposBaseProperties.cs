36d4b86555f125f25ee0e168179d827405e8a0af
Robert Snyder <immeraufdemhund@hotmail.com>
2015-03-28 12:42:48 -0500

    Fixed a problem with the default values for strings before Open is called.

--- Upos.ServiceObject/Upos.ServiceObject/Base/Properties/UposBaseProperties.cs
+++ Upos.ServiceObject/Upos.ServiceObject/Base/Properties/UposBaseProperties.cs
@@ -29,20 +29,20 @@ namespace Upos.ServiceObject.Base.Properties
             AddProperty("CapStatisticsReporting", PropertyConstants.PIDX_CapStatisticsReporting, 0);
             AddProperty("CapUpdateFirmware", PropertyConstants.PIDX_CapUpdateFirmware, 0);
             AddProperty("CapUpdateStatistics", PropertyConstants.PIDX_CapUpdateStatistics, 0);
-            AddProperty("CheckHealthText", PropertyConstants.PIDX_CheckHealthText, "");
+            AddProperty("CheckHealthText", PropertyConstants.PIDX_CheckHealthText, "[Error]");
             AddProperty("Claimed", PropertyConstants.PIDX_Claimed, 0);
             AddProperty("DataCount", PropertyConstants.PIDX_DataCount, 0);
             AddProperty("DataEventEnabled", PropertyConstants.PIDX_DataEventEnabled, 0);
-            AddProperty("DeviceDescription", PropertyConstants.PIDX_DeviceDescription, "");
+            AddProperty("DeviceDescription", PropertyConstants.PIDX_DeviceDescription, "[Error]");
             AddProperty("DeviceEnabled", PropertyConstants.PIDX_DeviceEnabled, 0);
-            AddProperty("DeviceName", PropertyConstants.PIDX_DeviceName, "");
+            AddProperty("DeviceName", PropertyConstants.PIDX_DeviceName, "[Error]");
             AddProperty("FreezeEvents", PropertyConstants.PIDX_FreezeEvents, 0);
             AddProperty("OutputID", PropertyConstants.PIDX_OutputID, 0);
             AddProperty("PowerNotify", PropertyConstants.PIDX_PowerNotify, 0);
             AddProperty("PowerState", PropertyConstants.PIDX_PowerState, 0);
             AddProperty("ResultCode", PropertyConstants.PIDX_ResultCode, ResultCodeConstants.Closed);
             AddProperty("ResultCodeExtended", PropertyConstants.PIDX_ResultCodeExtended, 0);
-            AddProperty("ServiceObjectDescription", PropertyConstants.PIDX_ServiceObjectDescription, "");
+            AddProperty("ServiceObjectDescription", PropertyConstants.PIDX_ServiceObjectDescription, "[Error]");
             AddProperty("ServiceObjectVersion", PropertyConstants.PIDX_ServiceObjectVersion, 0);
             AddProperty("State", PropertyConstants.PIDX_State, ServiceStateConstants.OPOS_S_CLOSED);
         }
