ae72f80affa873f69a52e5300dbde83115316b70
Robert Snyder <immeraufdemhund@hotmail.com>
2015-03-29 21:42:14 -0500

    Adding a test.. doesn't provide much value except for me to understand a tad better how delegates work.

---
+++ Upos.ServiceObject/Upos.ServiceObject.Tests/Base/UposEvents/UposEventArgumentsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Upos.ServiceObject.Base.UposEvents
+{
+    [TestFixture]
+    public class UposEventArgumentsTests
+    {
+        private delegate void Something(string ass);
+
+        [Test]
+        public void TestDelegates()
+        {
+            Something s = Console.WriteLine;
+
+            s("Hello");
+
+            s += Test;
+
+            s("Robert");
+
+            s = Test;
+
+            s("Berto");
+        }
+
+        private void Test(string me)
+        {
+            Console.WriteLine("Riiiigggghhhttt...'{0}'", me);
+        }
+    }
+}
