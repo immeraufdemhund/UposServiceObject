38fd7f93bc6a6725159b2e5c056274bb3f466110
Robert Snyder <immeraufdemhund@hotmail.com>
2016-06-25 06:11:00 -0500

    Cleaned up all AssemblyInfo files Updated Moq and FluentAssertions Added New Test project

---
+++ DemoServiceObjects.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DemoServiceObjects.IntegrationTests")]
+[assembly: AssemblyCompany("Robert Snyder")]
+[assembly: AssemblyProduct("DemoServiceObjects.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright © Robert Snyder 2016")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Upos.ServiceObject/Properties/AssemblyInfo.cs
+++ Upos.ServiceObject/Properties/AssemblyInfo.cs
@@ -1,33 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Upos Service Object Base")]
 [assembly: AssemblyDescription("A base library to help create a Upos Service Object")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Robert Snyder")]
 [assembly: AssemblyProduct("Upos Service Object Base")]
 [assembly: AssemblyCopyright("Copyright © Robert Snyder 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("edc51fd6-1857-4af9-93ea-b99f87bf78f7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
