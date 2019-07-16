0dd54f41a2a8a00e4f4afff70f1f68a21577a0e1
Robert Snyder <immeraufdemhund@hotmail.com>
2019-07-16 05:42:25 -0500

    *** WIP ***

--- Upos.ServiceObject.CashDrawer/CashDrawerControlObject.cs
+++ Upos.ServiceObject.CashDrawer/CashDrawerControlObject.cs
@@ -1,42 +1,40 @@
-﻿namespace Upos.ServiceObject.CashDrawer
+﻿using log4net;
+using Upos.ServiceObject.CashDrawer.Interfaces;
+
+namespace Upos.ServiceObject.CashDrawer
 {
-    internal class CashDrawerControlObject : Base.DynamicControlObject
+    public class CashDrawerControlObject : Base.DynamicControlObject
     {
-        private readonly Interfaces.COPOSCashDrawer _controlObject;
+        private static ILog Log = LogManager.GetLogger(typeof(CashDrawerControlObject));
+
+        private readonly ICashDrawerControlObject _controlObject;
 
         public CashDrawerControlObject(object dispatchObject)
         {
-            _controlObject = (Interfaces.COPOSCashDrawer) dispatchObject;
+            _controlObject = (ICashDrawerControlObject)dispatchObject;
         }
 
-        protected override void FireDataEvent(int status)
-        {
-            _controlObject.SOData(status);
-        }
-
-        protected override void FireDirectIOEvent(int eventNumber, ref int pData, ref string pString)
-        {
-            _controlObject.SODirectIO(eventNumber, ref pData, ref pString);
-        }
+        protected override void FireDataEvent(int status) { }
+        protected override void FireDirectIOEvent(int eventNumber, ref int pData, ref string pString) { }
+        protected override void FireOutputCompleteEvent(int outputId) { }
 
         protected override void FireErrorEvent(int resultCode, int resultCodeExtended, int errorLocus, ref int pErrorResponse)
         {
-            _controlObject.SOError(resultCode, resultCodeExtended, errorLocus, ref pErrorResponse);
-        }
-
-        protected override void FireOutputCompleteEvent(int outputId)
-        {
-            _controlObject.SOOutputComplete(outputId);
+            //_controlObject.SOError(resultCode, resultCodeExtended, errorLocus, ref pErrorResponse);
         }
 
         protected override void FireStatusUpdateEvent(int data)
         {
+            Log.DebugFormat("+Fire StatusUpdateEvent({0})", data);
             _controlObject.SOStatusUpdate(data);
+            Log.Debug("-Fire StatusUpdateEvent()");
         }
 
         protected override void GetProcessId(out int pProcessId)
         {
+            Log.DebugFormat("+Fire GetProcessId");
             _controlObject.SOProcessID(out pProcessId);
+            Log.DebugFormat("-Fire StatusUpdateEvent. set processId to {0}", pProcessId);
         }
     }
 }
---
+++ Upos.ServiceObject.CashDrawer/Interfaces/ICashDrawerControlObject.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Upos.ServiceObject.CashDrawer.Interfaces
+{
+    [ComImport, Guid("CCB94041-B81E-11D2-AB74-0040054C3719"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    internal interface ICashDrawerControlObject : ICashDrawerControlObject_1_9
+    {
+        [Description("method SOData"), DispId(1)]
+        void SODataDummy([In] int Status);
+
+        [Description("method SODirectIO"), DispId(2)]
+        void SODirectIO([In] int EventNumber, [In, Out] ref int pData, [In, Out, MarshalAs(UnmanagedType.BStr)]ref string pString);
+
+        [Description("method SOError"), DispId(3)]
+        void SOErrorDummy([In] int ResultCode, [In] int ResultCodeExtended, [In] int ErrorLocus, [In, Out] ref int pErrorResponse);
+
+        [Description("method SOOutputComplete"), DispId(4)]
+        void SOOutputCompleteDummy([In] int OutputID);
+
+        [Description("method SOStatusUpdate"), DispId(5)]
+        void SOStatusUpdate([In] int Data);
+
+        [Description("method SOProcessID"), DispId(9)]
+        void SOProcessID([Out] out int pProcessID);
+    }
+
+    [ComImport, Guid("CCB93041-B81E-11D2-AB74-0040054C3719"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    internal interface ICashDrawerControlObject_1_9 : ICashDrawerControlObject_1_8
+    {
+        [Description("method SOData"), DispId(1)]
+        void SODataDummy([In] int Status);
+
+        [Description("method SODirectIO"), DispId(2)]
+        void SODirectIO([In] int EventNumber, [In, Out] ref int pData, [In, Out, MarshalAs(UnmanagedType.BStr)]ref string pString);
+
+        [Description("method SOError"), DispId(3)]
+        void SOErrorDummy([In] int ResultCode, [In] int ResultCodeExtended, [In] int ErrorLocus, [In, Out] ref int pErrorResponse);
+
+        [Description("method SOOutputComplete"), DispId(4)]
+        void SOOutputCompleteDummy([In] int OutputID);
+
+        [Description("method SOStatusUpdate"), DispId(5)]
+        void SOStatusUpdate([In] int Data);
+
+        [Description("method SOProcessID"), DispId(9)]
+        void SOProcessID([Out] out int pProcessID);
+    }
+
+    [ComImport, Guid("CCB92041-B81E-11D2-AB74-0040054C3719"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    internal interface ICashDrawerControlObject_1_8 : ICashDrawerControlObject_1_5
+    {
+        [Description("method SOData"), DispId(1)]
+        void SODataDummy([In] int Status);
+
+        [Description("method SODirectIO"), DispId(2)]
+        void SODirectIO([In] int EventNumber, [In, Out] ref int pData, [In, Out, MarshalAs(UnmanagedType.BStr)]ref string pString);
+
+        [Description("method SOError"), DispId(3)]
+        void SOErrorDummy([In] int ResultCode, [In] int ResultCodeExtended, [In] int ErrorLocus, [In, Out] ref int pErrorResponse);
+
+        [Description("method SOOutputComplete"), DispId(4)]
+        void SOOutputCompleteDummy([In] int OutputID);
+
+        [Description("method SOStatusUpdate"), DispId(5)]
+        void SOStatusUpdate([In] int Data);
+
+        [Description("method SOProcessID"), DispId(9)]
+        void SOProcessID([Out] out int pProcessID);
+    }
+
+    [ComImport, Guid("CCB91041-B81E-11D2-AB74-0040054C3719"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    internal interface ICashDrawerControlObject_1_5
+    {
+        [Description("method SOData"), DispId(1)]
+        void SODataDummy([In] int Status);
+
+        [Description("method SODirectIO"), DispId(2)]
+        void SODirectIO([In] int EventNumber, [In, Out] ref int pData, [In, Out, MarshalAs(UnmanagedType.BStr)]ref string pString);
+
+        [Description("method SOError"), DispId(3)]
+        void SOErrorDummy([In] int ResultCode, [In] int ResultCodeExtended, [In] int ErrorLocus, [In, Out] ref int pErrorResponse);
+
+        [Description("method SOOutputComplete"), DispId(4)]
+        void SOOutputCompleteDummy([In] int OutputID);
+
+        [Description("method SOStatusUpdate"), DispId(5)]
+        void SOStatusUpdate([In] int Data);
+
+        [Description("method SOProcessID"), DispId(9)]
+        void SOProcessID([Out] out int pProcessID);
+    }
+}
